632057411449fc4e13868fb6796b609770b4e517
DUYONGBEAK <dybaek411@naver.com>
2021-04-09 17:28:02 +0900

    game

--- Project0408/src/Enemy.java
+++
@@ -1,7 +0,0 @@
-// 최상위 부모 클래스를 상속받는 Enemy 추상클래스 생성
-public abstract class Enemy extends Base {
-
-	// 각 보스가 공격하기 위해 사용할 메소드
-	// Base클래스 타입으로 player변수 생성
-	public abstract void attack(Base player);
-}
